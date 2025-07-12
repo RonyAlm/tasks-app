43762ab76d538348631b5cbc4baeded475ab21e3
Ronaldo Almirón <rony.almiron.015@gmail.com>
2025-07-12 18:28:54 -0300

    feat: initialize frontend with React, TypeScript Vite, and Tailwind CSS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="container w-full max-h-min h-full mx-auto bg-blue-950">
+      <h1 className="text-3xl font-bold underline">Tasks App</h1>
+    </div>
+  )
+}
+
+export default App
